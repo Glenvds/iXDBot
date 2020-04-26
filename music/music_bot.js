d885db5362e8ebb48629f03caea04817e8c1e4fa
Glen <glen.vandesteen@gmail.com>
2020-04-26 22:06:39 +0200

    radio try

--- music/music_bot.js
+++ music/music_bot.js
@@ -3,22 +3,42 @@ const ytsr = require('ytsr');
 //const radio = require('radio-stream');
 var icecast = require('icecast');
 
+const radioStations = [{name: "Studio Brussel", url: "http://icecast.vrtcdn.be/stubru-high.mp3"}, {name: "MNM", url:"http://icecast.vrtcdn.be/mnm-high.mp3"}, {name: "Radio 1", url:"http://icecast.vrtcdn.be/radio1-high.mp3"},
+ {name: "Klara", url:"http://icecast.vrtcdn.be/klara-high.mp3"}, {name: "TOPRadio", url:"http://stream4.topradio.be/topradio.mp3"}];
+
 class MusicBot {
     constructor() {
         this.queue = new Map();
+        this.isRadioPlaying = false;
+        this.isMusicPlaying = false;
     }
 
     executeMusicCommand(command, message) {
-        const serverQueue = this.queue.get(message.guild.id);
+        const serverQueue = this.queue.get(message.guild.id);        
         switch (command) {
             case "play": this.getSong(message, serverQueue); break;
             case "skip": this.skip(serverQueue); break;
-            case "next": console.log("NEXT COMMAND"); this.skip(serverQueue); break;
+            case "next": this.skip(serverQueue); break;
             case "stop": this.stop(message, serverQueue); break;
             case "queue": this.getQueue(serverQueue); break;
             case "radio": this.playRadio(message); break;
         }
     }
+
+
+
+    setupVoiceConnection(voiceChannel){
+        try{
+            let connection = voiceChannel.join();
+            return connection;
+        }
+        catch(err){
+            console.log("Error while setting up voice connection: " + err);
+            this.queue.delete(message.guild.id);
+        }
+        
+    }
+
 
     async getSong(message, serverQueue) {
         const voiceChannel = message.member.voice.channel;
@@ -52,17 +72,9 @@ class MusicBot {
             };
 
             this.queue.set(message.guild.id, queueContruct);
-
             queueContruct.songs.push(song);
-
-            try {
-                var connection = await voiceChannel.join();
-                queueContruct.connection = connection;
-                this.play(message.guild, queueContruct.songs[0]);
-            } catch (err) {
-                console.log(err);
-                this.queue.delete(message.guild.id);
-            }
+            queueContruct.connection = this.setupVoiceConnection(voiceChannel);
+            this.play(message.guild, queueContruct.songs[0]);
         }
         else {
             serverQueue.songs.push(song);
@@ -72,10 +84,12 @@ class MusicBot {
 
     async play(guild, song) {
         const serverQueue = this.queue.get(guild.id);
+        this.isMusicPlaying = true;
         if (!song) {
             this.sendMessageToChannel(serverQueue.textChannel, "Ran out of song, I'm leaving. Soai.")
             serverQueue.voiceChannel.leave();
             this.queue.delete(guild.id);
+            this.isMusicPlaying = false;
             return;
         }
 
@@ -104,9 +118,11 @@ class MusicBot {
     }
 
     stop(serverQueue) {
+        
         this.sendMessageToChannel(serverQueue.textChannel, "Deleted my queue, I'm out.")
         serverQueue.songs = [];
         serverQueue.connection.dispatcher.end();
+        this.isMusicPlaying = false;
     }
 
     getQueue(serverQueue) {
@@ -140,6 +156,7 @@ class MusicBot {
     }
 
     async playRadio(message) {
+        this.isRadioPlaying = true;
         const voiceChannel = message.member.voice.channel;
         const textChannel = message.channel;
         const url = "http://icecast.vrtcdn.be/radio1-high.mp3";
@@ -148,10 +165,10 @@ class MusicBot {
 
 
         const connection = await voiceChannel.join();
-
-        icecast.get(url, (res) => {
+        const dispatcher = connection.play(url);
+        /*icecast.get(url, (res) => {
             const dispatcher = connection.play(res);
-        })
+        })*/
     }
 
 
